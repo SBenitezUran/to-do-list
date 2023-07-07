f328bdcfb89ca3107f9a0601d6f02723a1e38d52
santiago <santiagobenitez467@gmail.com>
2023-07-07 15:07:20 -0500

    modified:controllers/status.js

--- controllers/status.js
+++ controllers/status.js
@@ -1,19 +1,7 @@
 const Status = require('../models/status')
 
 const controllerStatus = {
-  // create: async (req,res) => {
-  //   try {
-  //     const status = req.body.status
-  //     await Status.create ({
-  //       status : status
-  //     })
-  //     res.json({msg:'Created'})
-
-  //   } catch (error) {
-  //     return res.status(500).json ({msg:error.message})
-  //   }
-  // }
-
+  
   get: async (req,res) => {
     try {
       const status = await Status.find({})
@@ -53,16 +41,7 @@ const controllerStatus = {
     }
   },
 
-  // delete: async (req,res)=>{
-  //   try {
-  //       const {id} = req.params
-  //       await Status.findByIdAndDelete(id)
-  //       res.json({msg:'Deleted'})
-  //   } catch (err) {
-  //       console.error(err)
-  //       return res.status(500).json({msg:err.message})
-  //   }
-  // },
+  
 }
 
 module.exports = controllerStatus
